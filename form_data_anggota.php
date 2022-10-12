a7806f6285ed0b30025bc38f45d7c48201580903
Ihsan Magribi <ihsanmahgribi@gmail.com>
2022-10-13 00:32:34 +0800

    posttes4

--- form_data_anggota.php
+++ form_data_anggota.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/style.css">
     <title>Form Data Anggota</title>
+    <link rel="stylesheet" href="css/style_form.css">
     <link rel="icon" href="https://ipmboyolali.com/uploads/cover_blog/lambang_ipm.jpg">
 </head>
 <body>
@@ -65,5 +65,3 @@
     </form>
 </body>
 </html>
-<style>
-    
